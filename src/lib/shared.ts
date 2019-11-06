fba2b6ffe27de088674ebd03f51ec18851580093
Jonathan Olier <jonathan.olier@qrvey.com>
2019-11-06 09:44:54 -0500

    fix(*): the caseid must to have ":" at the end

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -6,7 +6,7 @@
 export function titleToCaseIds(title: string): number[] {
     let caseIds: number[] = [];
 
-    let testCaseIdRegExp: RegExp = /\bT?C(\d+)\b/g;
+    let testCaseIdRegExp: RegExp = /\bT?C(\d+)\b:/g;
     let m;
     while((m = testCaseIdRegExp.exec(title)) !== null) {
         let caseId = parseInt(m[1]);
